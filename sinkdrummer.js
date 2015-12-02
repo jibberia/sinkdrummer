6cfcb2bba43a0b06a621d91fa9389ae5129d3de4
Kevin Cox <kevintcox@gmail.com>
2015-12-02 15:22:42 -0800

    still flailing around setting up the basics

--- sinkdrummer.js
+++ sinkdrummer.js
@@ -1,20 +1,60 @@
-var context;
+"use strict";
 
-var sampleUrl = "/Users/kevin/Desktop/old-computer/a samples/sink_all_120bpm_44.aif";
+var context;
+// var buffer = null;
+var sampleUrl = "/samples/sink_all_120bpm_44.wav";
+// var sampleUrl = "/samples/verbme.aif";
+// var sampleUrl = "/samples/SupaTrigga_dry1.mp3";
+
+var loadAudio = function(url, onSuccess) {
+	var request = new XMLHttpRequest();
+	request.open('GET', url, true);
+	request.responseType = 'arraybuffer';
+
+	// Decode asynchronously
+	request.onload = function() {
+		console.log("request loaded, now decode...");
+
+		context.decodeAudioData(request.response,
+			function(buffer) {
+				console.log("successfully decoded", url);
+				onSuccess(buffer);
+			},
+			function(e) {
+				if (e) console.error("actual exception", e);
+				else console.error("failed to decode", url);
+			}
+		);
+
+		// context.decodeAudioData(request.response)
+		// 	.then(function(buffer) {
+		// 		console.log("audio decoded");
+		//   		window.buffer = buffer;
+		// 	});
+		// 	.then, function(e) {
+		// 	console.error(e.err);
+		// });
+
+	};
+
+	request.send();
+};
+
+var onLoaded = function(buffer) {
+	console.log("loaded", buffer);
+};
 
 var main = function() {
 	console.log("sinkdrummer says hi");
 	try {
-		context = new AudioContext();
+		window.AudioContext = window.AudioContext || window.webkitAudioContext;
+		window.context = new AudioContext();
 	} catch(e) {
-		console.error("Web Audio is not supported");
+		console.error("Web Audio is not supported, bailing");
+		return;
 	}
-
+	
+	loadAudio(sampleUrl, onLoaded);
 };
 
-var load = function(url) {
-
-}
-
 window.addEventListener('load', main);
-
